a456bdae6ebc9e088f8df4baa1aa158aa01defdd
simone-romiti <simone.romiti.1994@gmail.com>
2024-08-08 18:42:50 +0200

    some more physical constants

--- constants.py
+++ constants.py
@@ -4,14 +4,17 @@ import numpy as np
 
 ## masses
 
-masses = {
+masses_MeV = {
+    "e" : 0.5109989461,# electron
+    "mu": 105.6583745, # \mu lepton
+    "tau": 1776.86, # \tau lepton
     "pi": {
-        "+": 139.57061,
-        "0": 134.9770
+        "+": 139.57061, # \pi^+
+        "0": 134.9770   # \pi^0
     },
     "K": {
-        "+": 493.664,
-        "0": 497.611
+        "+": 493.664, # K^+
+        "0": 497.611  # K^0
     }
 }
 
@@ -22,6 +25,10 @@ GeV_to_MeV = lambda x: 1000.0*x
 MeV_inv_to_GeV_inv = lambda x: GeV_to_MeV(x)
 GeV_inv_to_MeV_inv = lambda x: MeV_to_GeV(x)
 
+# Conversion from fm to MeV^{-1}
+hbarc_fm_MeV = np.float64(197.3269804)
+
+fm_to_MeV_inv = lambda a: a/hbarc_fm_MeV
 
 ## electromagnetic fine structure constant
 alpha_EM_inv = 137.035999084
