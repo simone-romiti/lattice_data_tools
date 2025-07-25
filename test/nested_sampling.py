f11e83fd265ecf4ac6cb8ba807e704b7c974cf13
simone-romiti <simone.romiti.1994@gmail.com>
2025-07-25 19:11:45 +0200

    polished output of nested_sampling.py test script

--- test/nested_sampling.py
+++ test/nested_sampling.py
@@ -18,17 +18,16 @@ volume = Nt * int(Nx**3) # total volume
 d = 4 # number of spacetime dimensions
 n_plaq = d*(d-1)*volume # number of plaquettes
 
-n_live = 50
+n_live = 10000
 n_iter = 1000000 # NS iterations
 
 beta_gen = 5.0 # value of beta used only to generate the synthetic data
-# S = np.random.uniform(low=0.0, high=n_plaq, size=n_iter) # imagine you produced this data 
 n = 3
 Sn = np.sort(np.random.exponential(scale = 1.0/(beta_gen), size=n_iter)) # imagine you produced this data 
 S = np.power(Sn, 1/n) # like this, the distribution is: S^(n-1) * exp(-beta_gen*S^n)
 
 # phase space variables
-log_t = get_log_t(n_live=50, n_iter=n_iter, sampling_t=False)
+log_t = get_log_t(n_live=n_live, n_iter=n_iter, sampling_t=False)
 log_X = get_log_X(log_t = log_t)
 log_w = get_log_w(log_t=log_t, log_X=log_X, strategy="symm")
 
@@ -37,7 +36,7 @@ beta_target = 2.0
 log_L = (n-1)*np.log(S) -beta_target*Sn
 log_wL = get_log_wL_normalized(log_wL=get_log_wL_unnormalized(log_w=log_w, log_L=log_L))
 
-eps_wL = 1e-5 # pruning threshold
+eps_wL = 1e-8 # pruning threshold
 idx_pruned = get_idx_pruned_interval(log_wL_normalized=log_wL, eps_wL=eps_wL)
 log_X_pruned = log_X[idx_pruned]
 log_wL_pruned = log_wL[idx_pruned]
@@ -66,6 +65,7 @@ ax[1,1].set_xlabel("-$\\log{(X)}$")
 ax[1,1].set_ylabel("$Z$")
 
 
+plt.tight_layout()
 plt.savefig("./nested_sampling.pdf")
 # plt.show()
 
