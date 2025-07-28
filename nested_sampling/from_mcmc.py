62b6825f1a9e8caa959fe2f9739ab8849a613948
simone-romiti <simone.romiti.1994@gmail.com>
2025-07-28 12:15:59 +0200

    minor change: n_rows --> nrows (consistency with pandas)

--- nested_sampling/from_mcmc.py
+++ nested_sampling/from_mcmc.py
@@ -10,16 +10,16 @@ import numpy as np
 
 class Plaquette:
     @staticmethod
-    def read(path: str, n_rows: int = None, skiprows : int = None):
-        df = pd.read_csv(path, sep=" ", header=None, nrows=n_rows, skiprows=skiprows, dtype=np.float64)
+    def read(path: str, nrows: int = None, skiprows : int = None):
+        df = pd.read_csv(path, sep=" ", header=None, nrows=nrows, skiprows=skiprows, dtype=np.float64)
         P = df[1].to_numpy()
         return P
 #---
 
 class Polyakov:
     @staticmethod
-    def read(path: str, n_rows: int = None, skiprows : int = None):
-        df = pd.read_csv(path, sep=" ", header=None, nrows=n_rows, skiprows=skiprows, dtype=np.float64)
+    def read(path: str, nrows: int = None, skiprows : int = None):
+        df = pd.read_csv(path, sep=" ", header=None, nrows=nrows, skiprows=skiprows, dtype=np.float64)
         Re_P = df[0].to_numpy() 
         Im_P = df[1].to_numpy()
         polyakov = Re_P + 1j*Im_P
