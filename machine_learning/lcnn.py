32d6e08511e12bf1f0173e34f5a9ea8d60654e37
simone-romiti <simone.romiti.1994@gmail.com>
2026-04-21 23:59:04 +0200

    IN PROGRESS: LCE

--- machine_learning/lcnn.py
+++ machine_learning/lcnn.py
@@ -5,6 +5,7 @@ https://arxiv.org/abs/2012.12901
 """
 
 import torch
+from torch.nn import parallel
 
 def get_plaquettes(U: torch.Tensor):
     """
@@ -52,6 +53,55 @@ def get_Polyakov_loops(U: torch.Tensor):
             Poly[..., mu, :, :] @= torch.roll(U, -k, dims=1+mu)[..., mu, :,:] # P_\\mu(x) --> P_\\mu(x)*U_\\mu(x+mu)
     #-------
     return Poly
+#---
+
+def get_ParallelTransporters(k: int):
+    """
+    Returns the array of the parallel transporters \\prod_{i=0}^{j} U_\mu(x+i*\\mu) , j=0,...,k
+
+    Namely, the ones corresponding to a parallel transport along an axis, for up to "k" steps in the lattice spacing.
+    """
+    assert(U.is_complex())
+    d = U.shape[-3] # number of dimensions
+    lattice_shape = U.shape[1:-3] # shape of the lattice points grid
+    new_shape = tuple(U.shape[0:-2]) + (k,) + tuple(U.shape[-2:]) # shape: (batch_size,L1,...,Ld,d,k,3,3)
+    ParallelTransporters = U.unsqueeze(-2).expand(*new_shape).clone() # array of parallel transporters V_{\mu,i}(x)
+    for mu in range(d):
+        L_mu = lattice_shape[mu] # extension of the lattice over the \\mu-th direction
+        assert(k<=L_mu)
+        for i in range(1, k):
+            ParallelTransporters_fwd = torch.roll(ParallelTransporters, -i, dims=1+mu) # V_{\\mu,i}(x+i*\\mu)
+            ParallelTransporters[..., mu, i, :, :] @= ParallelTransporters_fwd[..., mu, i, :,:] # iterative contruction
+    #-------
+    return ParallelTransporters
+#---    
+
+class LCNN(torch.nn):
+    def __init__(self) -> None:
+        super().__init__()
+    #---
+
+    def get_W(U: torch.tensor):
+        """ List of locally transforming variables, as a tensor of shape (batch_size, n_variables, 3,3) """
+        Plaq = torch.flatten(get_plaquettes(U), start_dim=1, end_dim=-3) # all plaquettes. shape: (batch_size, n_plaq, 3,3)
+        Poly = torch.flatten(get_Polyakov_loops(U), start_dim=1, end_dim=-3) # all Polyakov loops. shape: (batch_size, n_poly, 3,3)
+        W = torch.stack((Plaq, Poly), dim=1)
+        return W
+    #---
+    
+    def LGE(W: torch.tensor, omega: torch.tensor, K: int):
+        """ Eq. 5 of https://arxiv.org/pdf/2012.12901"""
+        d = W.shape[-3] # number of dimensions
+        lattice_shape = W.shape[1:-3] # shape of the lattice points grid
+        W_conv = torch.zeros_like(W)
+        for k in range(1, K + 1):
+            ParallelTransporters = get_ParallelTransporters(k=k)
+            for mu in range(d):
+                W_shifted = torch.roll(W, shifts=-k, dims=1+mu)[...,mu,:,:] # W_\\mu(x+k*\\mu)
+                U_parall = ParallelTransporters[...,mu,k,:,:] # parallel transporters
+                W_transported = U_parall @ W_shifted @ (U_parall.adjoint()) #
+                W_out += torch.einsum('ijmk,bs...in->bs...on', omega, transported)
+                
 
 
 if __name__ == "__main__":
