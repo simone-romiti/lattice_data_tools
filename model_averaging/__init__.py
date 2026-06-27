c3b4f660258eba7ea16d48767c7192962d6dcae5
Simone Romiti <37484734+simone-romiti@users.noreply.github.com>
2026-06-27 11:29:26 +0200

    Update description for IC.py in __init__.py

--- model_averaging/__init__.py
+++ model_averaging/__init__.py
@@ -4,8 +4,7 @@ This folder contains scripts implementing Bayesian model averaging.
 **Available tools**:
 
 
-- `IC.py`: Model averaging using custom Information Criterion
-- `AIC.py`: Model averaging using the Akaike Information Criterion
+- `IC.py`: Model averaging using custom Information Criterion (e.g. Akaike Information Criterion)
 - `with_bts.py`: Model averaging using bootstrap samples
 
 """
